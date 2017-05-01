5b84dd281ab098e97ac709477b1b97400c470cd3
TT375S <tomataka375@gmail.com>
2017-05-01 17:21:56 +0900

    GoogleMaps関連のAPIを使うところが分散していたのを、クラスにまとめて解消(テストもまだ、アプリに組み込むのもまだ)

---
+++ app/src/main/java/com/example/user1/pacemakernavi/FusedLocationClientSingleton.java
@@ -0,0 +1,122 @@
+package com.example.user1.pacemakernavi;
+
+import android.app.Application;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.util.Log;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.FusedLocationProviderApi;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by user1 on 2017/05/01.
+ */
+
+public class FusedLocationClientSingleton extends Application implements
+        GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener,
+        LocationListener {
+
+    private static FusedLocationClientSingleton sInstance;
+    private LocationRequest locationRequest;
+    private FusedLocationProviderApi fusedLocationProviderApi;
+
+    // LocationClient の代わりにGoogleApiClientを使います
+    private GoogleApiClient mGoogleApiClient;
+
+    private boolean mResolvingError = false;
+
+    List<LocationListener> listeners = new ArrayList<LocationListener>(10);
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        sInstance = this;
+
+        // LocationRequest を生成して精度、インターバルを設定
+        locationRequest = LocationRequest.create();
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        locationRequest.setInterval(1000);
+        locationRequest.setFastestInterval(16);
+
+        fusedLocationProviderApi = LocationServices.FusedLocationApi;
+
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addApi(LocationServices.API)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .build();
+
+        startFusedLocation();
+    }
+
+    public static FusedLocationClientSingleton getInstance() {
+        return sInstance;
+    }
+
+    private void startFusedLocation() {
+        Log.d("NaviControlActivity", "onStart");
+
+        // Connect the client.
+        if (!mResolvingError) {
+            // Connect the client.
+            mGoogleApiClient.connect();
+        } else {
+        }
+    }
+
+    private void stopFusedLocation() {
+        // Disconnecting the client invalidates it.
+        mGoogleApiClient.disconnect();
+    }
+
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        fusedLocationProviderApi.requestLocationUpdates(mGoogleApiClient, locationRequest, this);
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Log.e("LocationClient", "ConnectionFailed!");
+        if (mResolvingError) {
+            // Already attempting to resolve an error.
+            Log.d("", "Already attempting to resolve an error");
+
+            return;
+        } else if (connectionResult.hasResolution()) {
+
+        } else {
+            mResolvingError = true;
+        }
+    }
+
+    //位置が変わったら呼ばれるメソッド
+    @Override
+    public void onLocationChanged(Location location) {
+    }
+
+    public void addListner(LocationListener listener) {
+        listeners.add(listener);
+    }
+
+
+}
---
+++ app/src/main/java/com/example/user1/pacemakernavi/GoogleMapsDirectionApiClient.java
@@ -0,0 +1,118 @@
+package com.example.user1.pacemakernavi;
+
+import android.os.AsyncTask;
+import android.util.Log;
+
+import com.google.android.gms.maps.model.LatLng;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+/**
+ * Created by user1 on 2017/05/01.
+ */
+
+//経路探索のためのクラス
+//インスタンスは作らず、GoogleMapsDirectionApiClient.fetchData(origin, destination, this);などとし、
+//呼び出し型でGoogleMapsDirectionApiReceiverインターフェースを実装し、コールバックメソッドonResultOfGoogleMapsDirectionApiを通し結果を受け取る
+public class GoogleMapsDirectionApiClient {
+    public interface GoogleMapsDirectionApiReceiver {
+        //結果を渡すためのコールバックメソッド
+        void onResultOfGoogleMapsDirectionApi(String result);
+    }
+
+    public void fetchData(LatLng origin, LatLng destination, GoogleMapsDirectionApiClient.GoogleMapsDirectionApiReceiver caller) {
+        if (origin == null || destination == null || caller == null) {
+            Log.e("DistanceMatrixClient", "ORIGN or DEST or CALLER is null!");
+            return;
+        }
+
+        //下の匿名クラスに渡すためにfinalで宣言したやつに入れ直す(回避の仕方がわかりません)
+        final GoogleMapsDirectionApiClient.GoogleMapsDirectionApiReceiver methodCaller = caller;
+
+        //非同期でGoogleDistanceMatrixAPIから情報をもらう
+        new AsyncTask<String, Void, String>() {
+            @Override
+            protected String doInBackground(String... strUrl) {
+                String data = "";
+                InputStream iStream = null;
+                HttpURLConnection urlConnection = null;
+                URL url = null;
+
+                try {
+                    url = new URL(strUrl[0]);
+
+                    // Creating an http connection to communicate with url
+                    urlConnection = (HttpURLConnection) url.openConnection();
+
+                    // Connecting to url
+                    urlConnection.connect();
+
+                    // Reading data from url
+                    iStream = urlConnection.getInputStream();
+
+                    BufferedReader br = new BufferedReader(new InputStreamReader(iStream));
+
+                    StringBuffer sb = new StringBuffer();
+
+                    String line = "";
+                    while ((line = br.readLine()) != null) {
+                        sb.append(line);
+                    }
+
+                    data = sb.toString();
+                    Log.d("downloadUrl", data.toString());
+                    br.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+                Log.d("Background Task data", data.toString());
+                return data;
+            }
+
+            @Override
+            protected void onPostExecute(String result) {
+                super.onPostExecute(result);
+                try {
+                    //デバッグのため情報表示
+                    JSONObject distanceResult = new JSONObject(result);
+                    JSONObject element = distanceResult.getJSONArray("rows").getJSONObject(0).getJSONArray("elements").getJSONObject(0);
+                    JSONObject distanceJson = element.getJSONObject("distance");
+                    JSONObject durationJson = element.getJSONObject("duration");
+                    Log.i("MainActivity", "DistanceMatrixAPI: DISTANCE: " + distanceJson.getString("text") + " DURATION: " + durationJson.getString("text"));
+
+                    //呼び出し元のコールバックメソッドを呼び出して、結果を渡す
+                    methodCaller.onResultOfGoogleMapsDirectionApi(result);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }.execute(generateUrl(origin, destination));
+
+    }
+
+    //リクエストのためのURL生成
+    private String generateUrl(LatLng origin, LatLng dest) {
+        // Origin of route
+        String str_origin = "origin=" + origin.latitude + "," + origin.longitude;
+        // Destination of route
+        String str_dest = "destination=" + dest.latitude + "," + dest.longitude;
+        // Sensor enabled
+        String sensor = "sensor=false";
+        // Building the parameters to the web service
+        String parameters = str_origin + "&" + str_dest + "&" + sensor;
+        // Output format
+        String output = "json";
+        // Building the url to the web service
+        String url = "https://maps.googleapis.com/maps/api/directions/" + output + "?" + parameters;
+
+        return url;
+    }
+}
